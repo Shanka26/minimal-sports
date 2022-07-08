d1ebc93565262640fd3357c4fab7678586a53464
Shanka26 <47332795+Shanka26@users.noreply.github.com>
2022-07-07 21:27:48 -0400

    url

--- news/urls.py
+++ news/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getNbaNews),
-    path('stories',views.getStories)
+    path('stories/nba',views.getStories),
+    path('stories/nba/<str:team>',views.getNbaTeam)
 ]
--- news/views.py
+++ news/views.py
@@ -13,6 +13,46 @@ def getNba():
     articles=soup.findAll('article')
     return articles
 
+def getNbaTeam(team):
+    url ="https://www.nba.com/news/"+team
+    req=requests.get(url)
+    soup=BeautifulSoup(req.content)
+    stories=[]
+    articles=soup.findAll('div',class_='Py(14px) Pos(r)')
+    for a in articles:
+        l=a.find('a',class_='js-content-viewer Fw(b) Fz(18px) Lh(21px) LineClamp(2,40px) Fz(16px)!--sm1024 Lh(17px)--sm1024 LineClamp(2,34px)--sm1024 mega-item-header-link Td(n) C(#0078ff):h C(#000) LineClamp(2,46px) LineClamp(2,38px)--sm1024 not-isInStreamVideoEnabled wafer-destroyed')
+        if l is not None:
+            link=l['href']
+        else: link=None
+
+        i=a.find('img',class_='W(100%) Trsdu(0s)! Bdrs(2px)')
+        if i is not None:
+            img=i['src']
+        else: img= None
+
+        c=a.find('div',class_='C(#959595) Fz(11px) D(ib) Mb(6px)').find('span')
+        if c is not None:
+            source=c.text
+        else: source= None
+        
+        t=a.find('h3',class_='Mb(5px)')
+        if t is not None:
+            title=t.text
+        else: title= None
+
+        s=a.find('p',class_='Fz(14px) Lh(19px) Fz(13px)--sm1024 Lh(17px)--sm1024 LineClamp(2,38px) LineClamp(2,34px)--sm1024 M(0)')
+        if s is not None:
+            body=s.text
+        else: body= None
+        stories.append({
+            'src':img,
+            'title':title,
+            'summary':body,
+            'link':link,
+            'source':source
+        })
+    return stories
+
 def getNbaNews(request):
     articles=getNba()
     srml=""
