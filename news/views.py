f913aeee623ff59f520d5bb27cf03aeeaa00e3e3
Shanka26 <47332795+Shanka26@users.noreply.github.com>
2022-07-08 10:21:05 -0400

    url

--- news/views.py
+++ news/views.py
@@ -7,10 +7,12 @@ from rest_framework.decorators import api_view
 
 
 def getNba():
-    url ="https://www.nba.com/news"
+    # url ="https://www.nba.com/news"
+    url ="https://sports.yahoo.com/nba/teams/"+'boston'
     req=requests.get(url)
     soup=BeautifulSoup(req.content)
-    articles=soup.findAll('article')
+    # articles=soup.findAll('article')
+    articles=soup.findAll('div',class_='Cf')
     return articles
 
 
@@ -60,19 +62,19 @@ def getNbaNews(request):
     articles=getNba()
     srml=""
     i=[]
-    for a in articles:
-        # a.find('h2')
-        i=a.find('img',class_='VideoThumbnail_image__3nEOl')
-        if i is not None:
-            srml+=i['src']
+    # for a in articles:
+    #     # a.find('h2')
+    #     i=a.find('img',class_='VideoThumbnail_image__3nEOl')
+    #     if i is not None:
+    #         srml+=i['src']
         
-        t=a.find('h2',class_='t1')
-        if t is not None:
-            srml+='<h2>'+t.text+'</h2>'
+    #     t=a.find('h2',class_='t1')
+    #     if t is not None:
+    #         srml+='<h2>'+t.text+'</h2>'
 
-        s=a.find('p',class_='t6 pt-2')
-        if s is not None:
-            srml+='<h4>'+s.text+'</h4>'
+    #     s=a.find('p',class_='t6 pt-2')
+    #     if s is not None:
+    #         srml+='<h4>'+s.text+'</h4>'
     
     return HttpResponse(articles)
 
