9e6c6eb5ad4cc20444771c2e04a702b638ba2b12
Shanka26 <47332795+Shanka26@users.noreply.github.com>
2022-07-09 20:46:54 -0400

    chromedriver

--- news/views.py
+++ news/views.py
@@ -10,6 +10,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.chrome.options import Options
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(chrome_options=chrome_options)
 # import pandas as pd
 
 
