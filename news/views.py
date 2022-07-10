4f40823140a0fc6cb0ba405b3ea619d568a4800a
Shanka26 <47332795+Shanka26@users.noreply.github.com>
2022-07-09 21:26:59 -0400

    chromedriver

--- news/views.py
+++ news/views.py
@@ -13,7 +13,7 @@ from selenium.webdriver.chrome.options import Options
 import os
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(chrome_options=chrome_options)
